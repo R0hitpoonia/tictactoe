c1cf5fca89fabbabfdd883738e31b9a7a86463cf
wolfie <wlfiesir@gmail.com>
2022-03-23 19:06:50 +0530

    checked everything again and calculated the winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,31 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Square from './Square'
 
-const Board = () => {
+const Board = ({board , handleSquareClick}) => {
   
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-
-  const handleSquareClick = (position) => {
-
-    if(board[ position ]) {
-      return;
-    }
-
-      setBoard( (prev) => {
-        
-        return prev.map((Square, pos) => {
-        if(pos === position) {
-          return isXNext ? 'X' : '0';
-        }
-
-        return Square;
-      })
-    })
-
-    setIsXNext(prev => !prev);
-  };
-
   const renderSquare = position => {
     return (
       <Square value={board[position]}
@@ -33,6 +10,7 @@ const Board = () => {
       />
     );
   };
+
 
   return (
     <div className="board">
